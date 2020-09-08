256e5b18381ee26dd354a14fc0c1d30a13eee762
yampoudel <rahulpaudel2015@outlook.com>
2020-09-08 11:25:00 +1000

    Fixed Header Link

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::get('/facades/decrypt', function () {
 });
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('generate/password', function () {
     return bcrypt('123456789');
